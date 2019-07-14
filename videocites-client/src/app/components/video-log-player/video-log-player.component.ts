926ccfb66e17404f9f58b6a9d5cc6f64b10d5d04
yinon <yinon.bentarya@gmail.com>
2019-07-15 01:17:00 +0300

    handle corrupted video

--- videocites-client/src/app/components/video-log-player/video-log-player.component.ts
+++ videocites-client/src/app/components/video-log-player/video-log-player.component.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { VideoLogItem } from 'src/app/models/video-log-item';
+import {
+  Component,
+  Input,
+  ViewChild,
+  OnChanges,
+  AfterViewInit,
+  AfterViewChecked
+} from "@angular/core";
+import { VideoLogItem } from "src/app/models/video-log-item";
 
 @Component({
-  selector: 'app-video-log-player',
-  templateUrl: './video-log-player.component.html',
-  styleUrls: ['./video-log-player.component.scss']
+  selector: "app-video-log-player",
+  templateUrl: "./video-log-player.component.html",
+  styleUrls: ["./video-log-player.component.scss"]
 })
-export class VideoLogPlayerComponent implements OnInit {
+export class VideoLogPlayerComponent implements AfterViewChecked, OnChanges {
   @Input() person: VideoLogItem;
-  constructor() { }
+  @ViewChild("video") matVideo: any;
+  video: HTMLVideoElement;
+  videoIsCorrupted = false;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnChanges() {
+    this.videoIsCorrupted = false;    
+    console.log("ngOnChanges: this.videoIsCorrupted ", this.videoIsCorrupted);
+    console.log("ngOnChanges: this.matVideo ", this.matVideo);
+    this.ngAfterViewChecked();
   }
 
+  ngAfterViewChecked() {
+    if (this.matVideo) {
+      this.video = this.matVideo.getVideoTag();
+      this.video.addEventListener("error", () => this.setVideoIsCorrupted());
+    }
+  }
+
+  setVideoIsCorrupted() {
+    this.videoIsCorrupted = true;
+    console.log("video corrupted flag is on");
+  }
 }
